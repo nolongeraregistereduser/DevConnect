b85f0701a8f923136adfcc8d5bfdb2d92678d2ed
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-02-25 11:28:46 +0000

    Rollback

--- routes/auth.php
+++ routes/auth.php
@@ -33,10 +33,6 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
-
-    Route::get('activate', function () {
-        return view('auth.activate');
-    })->name('auth.activate');
 });
 
 Route::middleware('auth')->group(function () {
