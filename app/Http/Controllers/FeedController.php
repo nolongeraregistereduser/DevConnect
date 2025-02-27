45197f77cc2458c006236a3f9d8cdd8d596e4462
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-02-27 10:10:43 +0000

    compact user & posts in view feed display feed dynamic

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -5,21 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Hashtag;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class FeedController extends Controller
 {
     public function index()
     {
-        $posts = Post::with(['user', 'comments.user', 'hashtags'])
-            ->withCount('likes')
-            ->latest()
-            ->paginate(10);
+
+        $user = Auth::user();
+        
+        $posts = Post::all();
 
         $trendingTags = Hashtag::orderBy('posts_count', 'desc')
             ->take(5)
             ->get();
 
-        return view('feed', compact('posts'));
+        return view('feed', compact('posts', 'user'));
 
     }
     }
