d780c474a1f8a87bab918988f79884f876d23ed9
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-02-28 11:57:40 +0000

    adding connection relationship

--- database/migrations/2025_02_25_110737_create_connections_table.php
+++ database/migrations/2025_02_25_110737_create_connections_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('connections', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('connected_user_id')->constrained('users')->onDelete('cascade');
+            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
             $table->timestamps();
+            $table->unique(['user_id', 'connected_user_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('connections');
